549a4b122b98304260513981bb1b678dccf88058
dikrivan91 <dikrivan91@gmail.com>
2023-02-07 10:37:22 +0700

    adding react-portofolio

---
+++ react-portofolio/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {FaBars, FaTimes} from 'react-icons'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline font-signature">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default Navbar
